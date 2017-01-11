71f7e663ad551645af3da139b56b6a27cdf987c7
Rob Wilkinson <wilkinson.robert.a@gmail.com>
2017-01-11 13:22:01 -0800

    knex migrate into npm

--- start.debug.sh
+++ start.debug.sh
@@ -3,5 +3,5 @@ echo "API container loaded, waiting for rabbitmq"
 while ! nc -z message-queue 5672; do sleep 3; done
 echo "Rabbitmq loaded"
 
-node_modules/.bin/knex migrate:latest
+npm run postinstall
 nodemon --debug=5858 index.js
