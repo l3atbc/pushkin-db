d55cfeed2c2773b31dbe80f4ee00365a107442d7
Rob Wilkinson <wilkinson.robert.a@gmail.com>
2017-01-11 17:39:26 -0800

    linting

--- index.js
+++ index.js
@@ -13,62 +13,57 @@ const Message = Bookshelf.Model.extend({
 });
 
 amqp.connect(process.env.AMPQ_ADDRESS).then(conn => {
-  process.once('SIGINT', function () {
+  process.once('SIGINT', () => {
     conn.close();
   });
   // Db Write
   return conn.createChannel().then(ch => {
     const q = 'db_queue';
-    const ok = ch.assertQueue(q, {
-      durable: true
-    });
+    const ok = ch.assertQueue(q, { durable: true });
     // prefetch due to multiple db writers
-    ok.then(() => {
-      ch.prefetch(1);
-    }).then(() => {
-      // consume messages
-      return ch.consume(q, function (msg) {
-        console.log(" [x] Received %s", msg.content.toString());
-        const message = new Message({
-          text: msg.content.toString()
-        });
+    ok
+      .then(() => {
+        ch.prefetch(1);
+      })
+      .then(() => {
+        // consume messages
+        return ch.consume(
+          q,
+          function(msg) {
+            console.log(' [x] Received %s', msg.content.toString());
+            const message = new Message({ text: msg.content.toString() });
 
-        return message.save().then(() => {
-          ch.ack(msg);
-        })
-      }, {
-        noAck: false
+            return message.save().then(() => {
+              ch.ack(msg);
+            });
+          },
+          { noAck: false },
+        );
       });
-    })
     return ok.then(() => {
-      console.log(" [*] Waiting for messages in %s. To exit press CTRL+C", q);
-    })
+      console.log(' [*] Waiting for messages in %s. To exit press CTRL+C', q);
+    });
   });
-})
+});
 amqp.connect(process.env.AMPQ_ADDRESS).then(conn => {
-
   return conn.createChannel().then(ch => {
-    process.once('SIGINT', function () {
+    process.once('SIGINT', () => {
       conn.close();
     });
     const q = 'db_read';
-    const ok = ch.assertQueue(q, {
-      durable: false
-    }).then(() => {
+    const ok = ch.assertQueue(q, { durable: false }).then(() => {
       ch.prefetch(1);
       return ch.consume(q, function reply(msg) {
-        const n = parseInt(msg.content.toString());
-        console.log(" [.] fib(%d)", n);
-        const r = n * 1000
         return Message.fetchAll().then(messages => {
-          ch.sendToQueue(msg.properties.replyTo,
-            new Buffer(JSON.stringify(messages)), {
-              correlationId: msg.properties.correlationId
-            });
+          ch.sendToQueue(
+            msg.properties.replyTo,
+            new Buffer(JSON.stringify(messages)),
+            { correlationId: msg.properties.correlationId },
+          );
           ch.ack(msg);
-        })
+        });
       });
-    })
+    });
     return ok.then(ch => {
       console.log(' [x] Awaiting RPC requests');
     });
