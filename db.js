5bd7aadb0702fe53b8d36c8e59e0559cf1c7e232
Rob Wilkinson <wilkinson.robert.a@gmail.com>
2017-01-23 18:09:23 -0800

    transaction db on every query response

--- db.js
+++ db.js
@@ -1,5 +1,24 @@
 const knex = require('knex')(require('./knexfile.js').development);
 
+const config = {
+  client: 'postgresql',
+  connection: 'postgres://postgres:postgres@transactiondb/transactions'
+};
+
+const db2 = require('knex')(config)
+
+db2.raw('select 1+1 as result').then(() => {
+  console.log('valid connection..');
+});
+
+knex.on('query-response', function(one, two, three) {
+  const obj = {
+    bindings: three.toSQL().bindings,
+    query: three.toSQL().sql,
+  };
+  return db2('transactions').insert(obj);
+});
+
 var fs = require('fs');
 var bookshelf = module.exports = require('bookshelf')(knex);
 var models = fs.readdirSync('./models');
