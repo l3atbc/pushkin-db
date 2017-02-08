0563404806facf81aadc5dcc3c9b1366bb788297
Rob Wilkinson <wilkinson.robert.a@gmail.com>
2017-02-07 16:41:30 -0800

    refactors docker file

--- start.debug.sh
+++ start.debug.sh
@@ -3,5 +3,6 @@ echo "API container loaded, waiting for rabbitmq"
 while ! nc -z message-queue 5672; do sleep 3; done
 echo "Rabbitmq loaded"
 npm run postinstall
+npm install -g nodemon
 
 nodemon --debug=5858 index.js
