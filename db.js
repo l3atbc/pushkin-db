e450f495da1727b68f6ab0998045958c5430d80e
Rob Wilkinson <wilkinson.robert.a@gmail.com>
2017-02-08 16:28:46 -0800

    Adds comments

--- db.js
+++ db.js
@@ -1,16 +1,22 @@
+// create the actual db connection
 const knex = require('knex')(require('./knexfile.js').development);
 
+
+// config for transaction DB
 const config = {
   client: 'postgresql',
   connection: 'postgres://postgres:postgres@transactiondb/transactions'
 };
 
+// create connection to transaction db
 const db2 = require('knex')(config)
 
+// test db connection
 db2.raw('select 1+1 as result').then(() => {
   console.log('valid connection..');
 });
 
+// whenever there is a query, save result in second db
 knex.on('query-response', function(one, two, three) {
   const obj = {
     bindings: three.toSQL().bindings,
@@ -19,12 +25,18 @@ knex.on('query-response', function(one, two, three) {
   return db2('transactions').insert(obj);
 });
 
+// instantiate bookshelf models
 var fs = require('fs');
 var bookshelf = module.exports = require('bookshelf')(knex);
-var models = fs.readdirSync('./models');
+
+// registry plugin to handle models that reference each other
+// more info available at: https://github.com/tgriesser/bookshelf/wiki/Plugin:-Model-Registry
 
 bookshelf.plugin('registry');
 
+// require all models and pass in db connection
+
+var models = fs.readdirSync('./models');
 models.forEach(function(model) {
   require('./models/' + model)(bookshelf);
 });
--- index.js
+++ index.js
@@ -1,57 +1,74 @@
 const amqp = require('amqplib');
-var db = require('./db');
-var winston = require('winston');
-
-
-const Trial = db.model('Trial');
-const TrialCollection = db.Collection.extend({
-  model: Trial
-})
+const winston = require('winston');
 const Worker = require('./worker');
-amqp.connect(process.env.AMPQ_ADDRESS).then(conn => {
-  return conn.createChannel().then(ch => {
-    process.once('SIGINT', function() {
-      conn.close();
-    });
-    const q = 'db_rpc_worker';
-    let ok = ch.assertQueue(q, { durable: false }).then(() => {
-      ch.prefetch(1);
-      return ch.consume(q, function reply(msg) {
-        var rpc = JSON.parse(msg.content.toString('utf8'));
-        winston.info('db_rpc_worker', {rpc, msg })
-        if(typeof Worker[rpc.method] === 'undefined') {
-          console.log(rpc);
-          throw new Error(`This method ${rpc.method} is not defined on the worker`);
-        }
-        return Worker[rpc.method].apply(Worker, rpc.arguments).then(data => {
-          winston.info(data)
-          ch.sendToQueue(
+
+const DB_WRITE_QUEUE = 'db_write';
+const DB_RPC_WORKER = 'db_rpc_worker';
+
+  // create the connection
+amqp.connect(process.env.AMPQ_ADDRESS).then((conn) => 
+  // create  seperate channel for this instance of the worker
+   conn.createChannel().then(ch => {
+    // on SIGINT ensure the channel is closed
+     process.once('SIGINT', function() {
+       conn.close();
+     });
+
+    // name of the queue
+    // this queue is not marked as durable, as it is an rpc
+    // if rabbitmq goes down, theres really nothing to replyTo
+     let ok = ch.assertQueue(DB_RPC_WORKER, { durable: false }).then(() => {
+      // set the maximum amount of messages that can be waiting before this channel accepts more
+      // more info here http://www.squaremobius.net/amqp.node/channel_api.html#channel_prefetch
+       ch.prefetch(1);
+      // consume on the specific queue
+       return ch.consume(DB_RPC_WORKER, (msg) => {
+        // parse the message into a javascript object
+         const rpc = JSON.parse(msg.content.toString('utf8'));
+         winston.info('db_rpc_worker', {rpc, msg })
+
+        //  check that this method is defined on the Worker
+         if (typeof Worker[rpc.method] === 'undefined') {
+           winston.error(rpc);
+          // throw an error otherwise
+          // TODO: put this in a seperate queue of bad messages.
+          // possible dead letter exchange: http://www.rabbitmq.com/dlx.html
+           throw new Error(`This method ${rpc.method} is not defined on the worker`);
+         }
+        // call the Worker with the defined method
+         return Worker[rpc.method].apply(Worker, rpc.arguments).then((data) => {
+           winston.info(data);
+          // send to the queue with the original replyTo, 
+          // pass it back with the correlation ID given
+           ch.sendToQueue(
             msg.properties.replyTo,
             new Buffer(JSON.stringify(data)),
             { correlationId: msg.properties.correlationId }
           );
-          ch.ack(msg);
-        });
-      });
-    });
-    ok = ok.then(() => {
-      return ch.assertQueue('db_write', { durable: true }).then(() => {
-        ch.prefetch(1);
-        return ch.consume('db_write', function reply(msg) {
-          winston.info('DB WRITE QUEUE', msg.content.toString('utf8'))
-          var rpc = JSON.parse(msg.content.toString('utf8'));
-          return Worker[rpc.method].apply(Worker, rpc.arguments).then(data => {
-            winston.info('was saved in db', data)
-            return ch.ack(msg);
-          })
-        })
-      })
-    });
+          // acknowledge receipt
+           ch.ack(msg);
+         });
+       });
+     });
+    // this queue is marked as durable, we want DB writes to persist through a rabbitmq crash
+     ok = ok.then(() => {
+       return ch.assertQueue(DB_WRITE_QUEUE, { durable: true }).then(() => {
+         ch.prefetch(1);
+         return ch.consume(DB_WRITE_QUEUE, (msg) => {
+           winston.info('DB WRITE QUEUE', msg.content.toString('utf8'))
+           const rpc = JSON.parse(msg.content.toString('utf8'));
+          // Data is returned but not used for anything
+           return Worker[rpc.method].apply(Worker, rpc.arguments).then(data => {
+             winston.info('was saved in db', data);
+             return ch.ack(msg);
+           });
+         });
+       });
+     });
 
-    return ok.then(ch => {
-      winston.log(' [x] Awaiting requests');
-    });
-  });
-});
+     return ok.then((ch) => {
+       winston.log(' [x] Awaiting requests');
+     });
+   }));
 
 // winston.handleExceptions(winston.transports);
