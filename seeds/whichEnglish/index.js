6c80fdcdccfb6d51c4f5f78cd3239096793931a8
Xiaolu Bai <icebunnydoll@gmail.com>
2017-05-04 15:04:00 -0700

    adds base schema folder "generalSchemas" for fs.copy made file names cleaner
    unified model names
    setup worker to accept different model instances
    removed general files
    
    moves which english seeds to its own folder
    
    updates JSDOC @examples
    var => const
    cleans up un used comments
    
    seeder file runs seeds for a quiz
    
    type changes in seeder.js
    fs.lstatsnyc to check if it is a folder not a file in db.js

--- seeds/whichEnglish/index.js
+++ seeds/whichEnglish/index.js
@@ -20,61 +20,66 @@ const trials = data[0].data;
 let questions = data[1].data;
 let choices = data[2].data;
 let languages = data[3].data;
-db
-  .knex('choices')
-  .del()
-  .then(() => {
-    return db.knex('questions').del();
-  })
-  .then(() => {
-    return db.knex('userLanguages').del();
-  })
-  .then(() => {
-    return db.knex('responses').del();
-  })
-  .then(() => {
-    return db.knex('users').del();
-  })
-  .then(() => {
-    return db.knex('trials').del();
-  })
-  .then(() => {
-    return db.knex('languages').del();
-  })
-  .then(() => {
-    return db.knex('trials').insert(trials).returning('*');
-  })
-  .then(rows => {
-    const allQuestions = rows.map(trial => {
-      return questions
-        .filter(question => question.trial === trial.name)
-        .map(question => {
-          question.trialId = trial.id;
-          delete question.trial;
-          return question;
-        });
+module.exports = () => {
+  return db
+    .knex('choices')
+    .del()
+    .then(() => {
+      return db.knex('questions').del();
+    })
+    .then(() => {
+      return db.knex('userLanguages').del();
+    })
+    .then(() => {
+      return db.knex('responses').del();
+    })
+    .then(() => {
+      return db.knex('users').del();
+    })
+    .then(() => {
+      return db.knex('trials').del();
+    })
+    .then(() => {
+      return db.knex('languages').del();
+    })
+    .then(() => {
+      return db.knex('trials').insert(trials).returning('*');
+    })
+    .then(rows => {
+      const allQuestions = rows.map(trial => {
+        return questions
+          .filter(question => question.trial === trial.name)
+          .map(question => {
+            question.trialId = trial.id;
+            delete question.trial;
+            return question;
+          });
+      });
+      return db.knex('questions').insert(flatten(allQuestions)).returning('*');
+    })
+    .then(rows => {
+      const allChoices = rows.map(question => {
+        return choices
+          .filter(choice => choice.question == question.prompt)
+          .map(choice => {
+            choice.questionId = question.id;
+            delete choice.question;
+            return choice;
+          });
+      });
+      return db.knex('choices').insert(flatten(allChoices)).returning('*');
+    })
+    .then(() => {
+      return db.knex('languages').insert(languages).returning('*');
+    })
+    .then(data => {
+      console.log(data);
+    })
+    .then(() => {
+      console.log('done seeding!');
+      return process.exit();
+    })
+    .catch(err => {
+      console.log(err);
     });
-    return db.knex('questions').insert(flatten(allQuestions)).returning('*');
-  })
-  .then(rows => {
-    const allChoices = rows.map(question => {
-      return choices
-        .filter(choice => choice.question == question.prompt)
-        .map(choice => {
-          choice.questionId = question.id;
-          delete choice.question;
-          return choice;
-        });
-    });
-    return db.knex('choices').insert(flatten(allChoices)).returning('*');
-  })
-  .then(() => {
-    return db.knex('languages').insert(languages).returning('*');
-  })
-  .then(data => {
-    console.log(data);
-    return process.exit();
-  })
-  .catch(err => {
-    console.log(err);
-  });
+};
