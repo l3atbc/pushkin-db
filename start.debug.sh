bdb4209559334adc48110d88c9fa9f5380b52d43
Rob Wilkinson <wilkinson.robert.a@gmail.com>
2017-05-05 17:33:32 -0700

    Db Worker now takes and applys methods from multiple quizzes

--- start.debug.sh
+++ start.debug.sh
@@ -2,7 +2,7 @@
 echo "API container loaded, waiting for rabbitmq"
 while ! nc -z message-queue 5672; do sleep 3; done
 echo "Rabbitmq loaded"
-npm install -g nodemon knex
-knex migrate:latest
+npm install -g --silent nodemon
+npm run migrations
 
 nodemon --debug=5858 index.js
