c97922930c55d454d3acae7f048aaef98c5810f8
Rob Wilkinson <wilkinson.robert.a@gmail.com>
2017-05-18 12:17:46 -0700

    connection pooling on db

--- knexfile.js
+++ knexfile.js
@@ -4,9 +4,13 @@ module.exports = {
   development: {
     client: 'postgresql',
     connection: process.env.DATABASE_URL,
+    pool: {
+      min: 2,
+      max: 10
+    },
     migrations: {
       tableName: 'knex_migrations'
-    },
+    }
   },
 
   staging: {
